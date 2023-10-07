20bc35ef332d50488fe3377aa309392038992144
elnickp27 <el.nickp27@gmail.com>
2023-10-07 02:33:39 -0400

    Added enemies with functioning AI and state machine

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    Transform target;
+    NavMeshAgent agent;
+    int currentNode;
+    int previousNode;
+
+    public enum EnemyState
+    {
+        patrol,
+        chase
+    };
+
+    EnemyState enemyState = EnemyState.patrol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        currentNode = Random.Range(0, GameManager.gm.nodes.Length);
+        previousNode = currentNode;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (enemyState)
+        {
+            case EnemyState.patrol: Patrol(); break;
+            case EnemyState.chase: Chase(); break;
+            default: break;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Node")
+        {
+            currentNode = Random.Range(0, GameManager.gm.nodes.Length);
+            while (currentNode == previousNode)
+            {
+                currentNode = Random.Range(0, GameManager.gm.nodes.Length);
+            }
+            previousNode = currentNode; 
+        }
+        else if (other.tag == "Player")
+        {
+            enemyState = EnemyState.chase;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            enemyState = EnemyState.patrol;
+        }
+    }
+
+    void Patrol()
+    {
+        agent.destination = GameManager.gm.nodes[0].position;
+    }
+
+    void Chase()
+    {
+        agent.destination = GameManager.gm.player.transform.position;
+    }
+}
